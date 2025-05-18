9b59ca9f93c5d65dcee39905f2a6135ff21cd9f0
mtyrolski <michal.tyrolski@gmail.com>
2025-05-18 19:37:44 +0200

    starting point refined

--- carl/run.py
+++ carl/run.py
@@ -1,32 +1,54 @@
 import os
-import pathlib
 import pickle
 import sys
 
 import hydra
 import torch
-from carl.utils.loggers import NeptuneCaRLLogger
+from carl.algorithms.algorithm import Algorithm
 from carl.utils.result_loggers import SubgoalSearchResultLogger
 from dotenv import load_dotenv
 from loguru import logger
 from neptune.utils import stringify_unsupported
 from omegaconf import DictConfig
 from omegaconf import OmegaConf
-from transformers.integrations import NeptuneCallback
 
 
+HIGHEST_PROTOCOL = 5
+pickle.HIGHEST_PROTOCOL = HIGHEST_PROTOCOL
 DOTENV_PATH = './.tokens.env'
+logger.info(F'Ensuring pickle.HIGHEST_PROTOCOL is set to {HIGHEST_PROTOCOL}')
 
-logger.info('Ensuring pickle.HIGHEST_PROTOCOL 5')
-pickle.HIGHEST_PROTOCOL = 5
+def handle_logging(algorithm: Algorithm, config: OmegaConf, logger_key_name: str = 'result_logger'):
+    pwd = os.getcwd()
+    real_pwd = os.environ.get('NEPTUNEPWD')
+    if not hasattr(algorithm, logger_key_name):
+        logger.info(f'No {logger_key_name} found in algorithm, skipping logging to external services.')
+        return
+    
+    assert isinstance(getattr(algorithm, logger_key_name), SubgoalSearchResultLogger), \
+        f'Expected {logger_key_name} to be of type NeptuneCaRLLogger, but got {type(getattr(algorithm, logger_key_name))}'
 
+    conf_to_log = OmegaConf.to_container(config)
+    algorithm.result_logger.custom_logger.run['parameters'] = stringify_unsupported(conf_to_log)
+    algorithm.result_logger.custom_logger.run['experiment_path'] = stringify_unsupported({
+        'pwd': pwd,
+        'real_pwd': real_pwd,
+    })
+    
+def handle_precision(algorithm: Algorithm, config: OmegaConf):
+    if config.get('float32_matmul_precision', None) is not None:
+        logger.info(f'Setting float32_matmul_precision to {config.float32_matmul_precision}')
+        torch.set_float32_matmul_precision(config.float32_matmul_precision)
 
+            
 def run(config: DictConfig) -> None:
     logger.info(OmegaConf.to_yaml(config))
     load_dotenv(DOTENV_PATH, override=True)
     logger.info(f'CUDA_VISIBLE_DEVICES: {os.environ.get("CUDA_VISIBLE_DEVICES")}')
     logger.info(f'NEPTUNE_API_TOKEN: {os.environ.get("NEPTUNE_API_TOKEN")}')
-
+    logger.remove()
+    logger.add(sys.stderr, level='INFO')
+    logger.add(sink=lambda msg: print(msg, end=''), level='INFO')
     # Check NEPTUNE_API_TOKEN
     if os.environ.get('NEPTUNE_API_TOKEN') is None:
         logger.error('NEPTUNE_API_TOKEN is not set')
@@ -34,47 +56,10 @@ def run(config: DictConfig) -> None:
 
     algorithm = hydra.utils.instantiate(config.algorithm)
     logger.info(f'Registered algorithm: {algorithm}')
-    logger.add(sink=lambda msg: print(msg, end=''), level='INFO')
+    
+    handle_logging(algorithm, config)
+    handle_precision(algorithm, config)
 
-    # TODO: remove this hack
-    # TODO: this is so ugly. Future me, please forgive me.
-
-    # for eval
-    if hasattr(algorithm, 'result_logger'):
-        if isinstance(algorithm.result_logger, SubgoalSearchResultLogger):
-            conf_to_log = OmegaConf.to_container(config)
-            algorithm.result_logger.custom_logger.run['parameters'] = stringify_unsupported(conf_to_log)
-            algorithm.result_logger.custom_logger.run['experiment_path'] = stringify_unsupported({
-                'pwd': os.getcwd(),
-                'real_pwd': os.environ.get('NEPTUNEPWD')
-            })
-
-    # for training
-    if hasattr(algorithm, 'custom_logger'):
-        if isinstance(algorithm.custom_logger, NeptuneCaRLLogger):
-            conf_to_log = OmegaConf.to_container(config)
-            algorithm.custom_logger.run['parameters'] = stringify_unsupported(conf_to_log)
-            algorithm.custom_logger.run['experiment_path'] = stringify_unsupported({
-                'pwd': os.getcwd(),
-                'real_pwd': os.environ.get('NEPTUNEPWD')
-            })
-
-    # rl loop
-    if hasattr(algorithm, 'neptune_logger'):
-        if isinstance(algorithm.neptune_logger, NeptuneCallback):
-            conf_to_log = OmegaConf.to_container(config)
-            algorithm.neptune_logger.run['parameters'] = stringify_unsupported(conf_to_log)
-            algorithm.neptune_logger.run['experiment_path'] = stringify_unsupported({
-                'pwd': os.getcwd(),
-                'real_pwd': os.environ.get('NEPTUNEPWD')
-            })
-
-    if config.get('float32_matmul_precision', None) is not None:
-        logger.info(f'Setting float32_matmul_precision to {config.float32_matmul_precision}')
-        torch.set_float32_matmul_precision(config.float32_matmul_precision)
-
-    logger.remove()
-    logger.add(sys.stderr, level='INFO')
     logger.info('Setting recursion limit to 2147483640')
     sys.setrecursionlimit(2147483640)
 
